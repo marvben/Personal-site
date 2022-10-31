5e30910a390e1238b90dd039a655254c9917a552
unknown <benluvforu@gmail.com>
2022-10-31 22:39:35 +0100

    add correct port

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const routes = require("./routes/routes");
+const port = PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", routes);
@@ -11,7 +12,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
   if (err) {
     console.log(err);
   }
