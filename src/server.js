74e31f6cfb96bdf29c83cabfd469189483a7986f
unknown <benluvforu@gmail.com>
2022-10-31 22:44:18 +0100

    add correct port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const routes = require("./routes/routes");
-const port = PORT || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", routes);
